4dbfde915ef7006c2e12af710912f66972bf5cd5
bodnargabor86 <bodnargabor97]mail.com>
2021-07-24 15:52:55 +0200

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+from PySide6.QtWidgets import QWidget, QApplication
+import sys
+
+app = QApplication(sys.argv)
+
+my_window = QWidget()
+my_window.show()
+
+app.exec()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
