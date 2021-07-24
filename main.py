b67239023460b540a4963bd4fb031a2beb691eef
bodnargabor86 <bodnargabor97]mail.com>
2021-07-24 20:42:04 +0200

    egész

--- main.py
+++ main.py
@@ -1,63 +1,91 @@
-from PySide6.QtWidgets import QWidget, \
-    QPushButton, QApplication, QLineEdit, QColormap, QTextEdit, QVBoxLayout, QHBoxLayout
-import sys, json
+from PySide6.QtWidgets import QWidget, QApplication, QLineEdit, \
+    QPushButton, QVBoxLayout, QTextEdit
+import sys, json, os
+
+DATA_FILE_PATH = "user_data.json"
+
 
 class RegistrationForm(QWidget):
     def __init__(self):
-        super(RegistrationForm, self).__init__()
+        super().__init__()
 
         # window config
         self.setWindowTitle("Registration Form")
-        self.resize(400,0)
+        self.resize(400, 0)
 
-        # window contents (widgets)
+        # window content (widgets)
         main_layout = QVBoxLayout(self)
 
-        #first_name
+        # first name
         self.first_name_field = QLineEdit()
-        self.first_name_field.setPlaceholderText("Írd be a keresztneved!")
+        self.first_name_field.setPlaceholderText("First name")
         main_layout.addWidget(self.first_name_field)
 
-        #last_name
+        # last name
         self.last_name_field = QLineEdit()
-        self.last_name_field.setPlaceholderText("Írd be a családi neved!")
+        self.last_name_field.setPlaceholderText("Last name")
         main_layout.addWidget(self.last_name_field)
 
-        #phone
+        # phone
         self.phone_field = QLineEdit()
-        self.phone_field.setPlaceholderText("Telószám")
+        self.phone_field.setPlaceholderText("Phone")
         main_layout.addWidget(self.phone_field)
 
-        #address
+        # address
         self.address_field = QLineEdit()
-        self.address_field.setPlaceholderText("Mi a címed?")
+        self.address_field.setPlaceholderText("Address")
         main_layout.addWidget(self.address_field)
 
+        # email
+        self.email_field = QLineEdit()
+        self.email_field.setPlaceholderText("Email")
+        main_layout.addWidget(self.email_field)
 
-        #save_button
+        # about
+        self.about_field = QTextEdit()
+        main_layout.addWidget(self.about_field)
+
+        # save button
         save_button = QPushButton("Save")
         main_layout.addWidget(save_button)
 
+        # load saved data
+        self.load_action()
 
-        # connect signals
+        # connect signals/slot mechanism
         save_button.clicked.connect(self.save_action)
-        self.first_name_field.textChanged.connect(self.first_name_changed)
 
     def save_action(self):
-        # print(f"First name: {self.first_name_field.text()}")
-        # print(f"Last name: {self.last_name_field.text()}")
+        # get data from fields
         data_dictionary = {
             "first_name": self.first_name_field.text(),
             "last_name": self.last_name_field.text(),
             "phone": self.phone_field.text(),
-            "address": self.address_field.text(),
+            "email": self.email_field.text(),
+            "address": self.address_field.text()
         }
 
-        print(data_dictionary)
+        # write out data
+        with open(DATA_FILE_PATH, "w") as f:
+            json.dump(data_dictionary, f)
 
+        # clear fields
+        self.first_name_field.clear()
+        self.last_name_field.clear()
+        self.phone_field.clear()
+        self.email_field.clear()
+        self.address_field.clear()
 
-    def first_name_changed(self, value):
-        print(value)
+    def load_action(self):
+        if os.path.exists(DATA_FILE_PATH):
+            with open(DATA_FILE_PATH) as f:
+                user_data = json.load(f)
+
+                self.first_name_field.setText(user_data["first_name"])
+                self.last_name_field.setText(user_data["last_name"])
+                self.email_field.setText(user_data["email"])
+                self.phone_field.setText(user_data["phone"])
+                self.address_field.setText(user_data["address"])
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -66,19 +94,3 @@ if __name__ == '__main__':
     win.show()
 
     app.exec()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
